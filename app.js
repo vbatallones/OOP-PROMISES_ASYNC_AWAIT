becbaf1a96f855675595ed9446add407382165e6
Levin Batallones <levinbatallones@Batallones.local>
2020-07-23 14:45:45 -0700

    [constructor] Add Constructor functions

--- app.js
+++ app.js
@@ -40,21 +40,68 @@
 
 //----------------------------------------
 //my own class
-const shoes = {
-    name: "jordan 1",
-    fake: false,
-    artist: 'Travis Scott'
+// const shoes = {
+//     name: "jordan 1",
+//     fake: false,
+//     artist: 'Travis Scott'
+// }
+
+// const shoesTwo = {
+//     name: 'Adidas',
+// }
+
+// shoesTwo.__proto__ = shoes;
+// console.log(shoesTwo)
+
+// console.log(shoesTwo.fake)
+
+
+// shoesTwo.fake = true;
+// console.log(shoesTwo.fake)
+
+//---------------------------------------------
+
+//CONSTRUCTOR FUNCTION
+
+//WHENEVER YOU USE CONSTRUCTION FUNCTION THE FIRST
+//CHAR NEEDS TO BE CAPITALIZE
+//this. is referring to the user
+//everytime we have a function we have to return something
+function User(name) {
+    //
+    this.name = name;
+
+    //return this
 }
 
-const shoesTwo = {
-    name: 'Adidas',
+let adam = new User('Adam');
+
+console.log(adam.name)
+
+function NBAPlayer(name, team, threePointShooter) {
+    this.name = name;
+    this.team = team;
+    this.threePointShooter = threePointShooter;
+
+    //no need to return because it is already return the object by the syntax
+    // this.
 }
 
-shoesTwo.__proto__ = shoes;
-console.log(shoesTwo)
+let steph = new NBAPlayer('Steph Curry', 'Warriors', true)
 
-console.log(shoesTwo.fake)
+console.log(steph)
+console.log(steph.name)
 
+//-----------------------------------------
 
-shoesTwo.fake = true;
-console.log(shoesTwo.fake)
+function Cars (name, brand, allWheelDrive) {
+    this.name = name;
+    this.brand = brand;
+    this.allWheelDrive = allWheelDrive
+}
+
+let toyota = new Cars("FRZ-86", "Toyota", false)
+
+console.log(toyota)
+console.log(toyota.brand)
+console.log(toyota.allWheelDrive)
